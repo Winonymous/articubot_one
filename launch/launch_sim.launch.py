2cc89aa3955813dc9b7b2897fca7e717ef88f944
Winonymous <anjuwonololade@gmail.com>
2024-02-21 00:51:53 +0100

    Add Ros Control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,14 +2,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
@@ -22,7 +19,9 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 
+                                       "use_camera": "true", 
+                                       "use_ros2_control": "true"}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -36,7 +35,15 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
 
+    diff_cont = Node(package="controller_manager",
+                     executable="spawner",
+                     arguments=["diff_cont"])
+    
+    joint_broad = Node(package="controller_manager",
+                     executable="spawner",
+                     arguments=["joint_broad"])
 
 
     # Launch them all!
@@ -44,4 +51,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_cont,
+        joint_broad
     ])
