299335cbc1733165a2b2ceb7e2b4ac276db015d0
Winonymous <anjuwonololade@gmail.com>
2024-02-22 13:25:51 +0100

    SLAM and NAV

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -44,12 +44,19 @@ def generate_launch_description():
     joint_broad = Node(package="controller_manager",
                      executable="spawner",
                      arguments=["joint_broad"])
+    
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(package="twist_mux",
+                     executable="twist_mux",
+                     parameters=[twist_mux_params, {"use_sim_time":True}],
+                     remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")])
 
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
+        twist_mux,
         spawn_entity,
         diff_cont,
         joint_broad
